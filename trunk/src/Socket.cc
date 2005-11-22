9adbf7485154315d4999c0ed2c1cd5613723d666
alex <alex@0e9dafd1-d605-0410-a1ff-fb02f8364bd5>
2005-11-22 15:31:40 +0000

    removed static methods

--- trunk/src/Socket.cc
+++ trunk/src/Socket.cc
@@ -6,26 +6,6 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-static ssize_t RECV(int fd,void* b,size_t len,int f) {
-    return recv(fd,b,len,f);
-}
-
-static ssize_t SEND(int fd,const void* b,size_t len,int f) {
-    return send(fd,b,len,f);
-}
-
-static ssize_t SENDTO(int fd,const void* b,size_t len, int flags, const struct sockaddr* to, size_t tolen) {
-    return sendto(fd, b, len, flags, to, tolen);
-}
-
-static ssize_t RECVFROM(int fd,void* b,size_t len,int flags,struct sockaddr* from, size_t* fromlen) {
-    return recvfrom(fd,b,len,flags,from,fromlen);
-}
-
-static int BIND(int fd, const struct sockaddr* addr, socklen_t len) {
-    return bind(fd,addr,len);
-}
-
 Socket::Socket() {
     _fd = -1;
 }
@@ -55,19 +35,19 @@ Socket::~Socket() {
 }
 
 ssize_t Socket::recv(void* buffer, size_t len, int flags) {
-    return RECV(_fd, buffer, len, flags);
+    return ::recv(_fd,buffer,len,flags);
 }
 
 ssize_t Socket::send(const void* buffer, size_t len, int flags) {
-    return SEND(_fd, buffer, len, flags);
+    return ::send(_fd, buffer, len, flags);
 }
 
 ssize_t Socket::recvfrom(void* buffer, size_t len, struct sockaddr *from, size_t* fromlen, int flags) {
-    return RECVFROM(_fd, buffer, len, flags, from, fromlen);
+    return ::recvfrom(_fd, buffer, len, flags, from, fromlen);
 }
 
 ssize_t Socket::sendto(const void* buffer, size_t len, const struct sockaddr *to, int flags) {
-    return SENDTO(_fd, buffer, len, flags, to, sizeof(struct sockaddr));
+    return ::sendto(_fd, buffer, len, flags, to, sizeof(struct sockaddr));
 }
 
 int Socket::bind(std::string hostorip, int port) {
@@ -80,5 +60,5 @@ int Socket::bind(std::string hostorip, int port) {
     }
     memset(&(_addr.sin_zero), '\0', 8);
 
-    return BIND(_fd,(struct sockaddr*)&_addr,sizeof(struct sockaddr));
+    return ::bind(_fd,(struct sockaddr*)&_addr,sizeof(struct sockaddr));
 }

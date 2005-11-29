3c2f3feefe6f86607806c2c1722d5dccecb1229b
alex <alex@0e9dafd1-d605-0410-a1ff-fb02f8364bd5>
2005-11-29 16:56:53 +0000

    added pidfile support

--- trunk/src/ipna-ft-fanout.cc
+++ trunk/src/ipna-ft-fanout.cc
@@ -1,12 +1,15 @@
 #include <iostream>
 #include <sstream>
-
+#include <fstream>
+#include <algorithm>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/file.h>
 #include <netdb.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
 
 #include <boost/program_options.hpp>
 #include <boost/algorithm/string.hpp>
@@ -19,9 +22,14 @@
 namespace po = boost::program_options;
 using namespace std;
 
+#define DEBUG(msg) if (verbosity) { cerr << "DEBUG: " << msg << endl; }
+#define ERROR(msg) cerr << "ERROR: " << msg << endl;
+
 typedef int Port;
 typedef string Ip;
 typedef pair<Ip, Port> IpPortPair;
+
+int verbosity;
 
 bool checkIpPortPair(IpPortPair& p) {
     Ip ip = p.first;
@@ -69,51 +77,92 @@ splitIntoIpPortPair(const string& s) {
 }
 
 int main(int argc, char** argv) {
-    int verbosity;
+    string pidfile;
 
     // 1. parse options
     // 2. listen on specified source
     // 3. loop:
     //    send packets to specified destinations
     po::options_description desc("Allowed options");
-    desc.add_options()
-        ("help", "show this help message")
-        ("verbose,v", "verbose output, debuglevel = number of -v")
-        ("ipv4,4", "use ipv4")
-        ("ipv6,6", "use ipv6, that is the default but currently not implemented!")
-        ("only-from,o", po::value<string>(), "fan out packets only from this source ip")
-        ("listen,l", po::value<string>(), "listen on ip/port")
-        ("export,e", po::value< vector<string> >(), "export to these probes (ip/port)")
-        ;
     po::positional_options_description p;
-    p.add("export", -1);
     po::variables_map vm;
-    po::store(po::command_line_parser(argc,argv).options(desc).positional(p).run(), vm);
-    po::notify(vm);
+
+    try {
+        desc.add_options()
+            ("help,h", po::value< vector<bool> >()->zero_tokens(), "show this help message")
+            ("pidfile,p", po::value<string>(), "use this file to store the pid")
+            ("verbose,v", po::value< vector<int> >()->implicit()->composing(), "verbose output")
+            ("ipv4,4", "use ipv4")
+            ("ipv6,6", "use ipv6, that is the default but currently not implemented!")
+            ("only-from,o", po::value<string>(), "fan out packets only from this source ip")
+            ("listen,l", po::value<string>(), "listen on ip/port")
+            ("export,e", po::value< vector<string> >(), "export to these probes (ip/port)")
+            ;
+        p.add("export", -1);
+    } catch (po::error & ex) {
+        cerr << ex.what() << endl;
+    }
+
+    try {
+        po::store(po::command_line_parser(argc,argv).options(desc).positional(p).run(), vm);
+        po::notify(vm);
+    } catch (po::error & ex) {
+        cerr << ex.what() << endl;
+        cerr << desc << endl;
+        exit(1);
+    } catch (...) {
+        ERROR("unknown things happened!");
+        exit(1);
+    }
+
+    verbosity = 0;
+    if (vm.count("verbose")) {
+        verbosity = 1;
+    }
 
     if (vm.count("help")) {
         cerr << desc << endl;
         exit(0);
     }
 
+    if (0 == vm.count("pidfile")) {
+        vector<string> splitted;
+        boost::split(splitted, argv[0], boost::algorithm::is_any_of("/"));
+        pidfile = string("/tmp/").append(splitted[splitted.size()-1]).append(".pid");
+    } else {
+        pidfile = vm["pidfile"].as<string>();
+    }        
+
+    DEBUG("using pidfile: \"" << pidfile << "\"");
+
+    try {
+        ofstream _pid_fstream(pidfile.c_str());
+        if (!_pid_fstream.is_open()) {
+            throw string("could not open file: ").append(pidfile.c_str());
+        }
+        _pid_fstream << getpid() << endl;
+        _pid_fstream.close();
+    } catch (string & ex) {
+        ERROR("could not write pid: " << ex);
+        exit(1);
+    }
+    
     if (0 == vm.count("listen")) {
-        cerr << "ERROR: no ip/port pair specified to listen on!" << endl;
+        ERROR("no ip/port pair specified to listen on!");
         cerr << desc << endl;
         exit(1);
     }
 
     if (0 == vm.count("export")) {
-        cerr << "ERROR: no ip/port pair specified to export to!" << endl;
+        ERROR("no ip/port pair specified to export to!");
         cerr << desc << endl;
         exit(2);
     }
 
-    verbosity = vm.count("verbose");
-
     // parse listen pair and check it
     IpPortPair listenPair = splitIntoIpPortPair( vm["listen"].as<string>() );
     if (!checkIpPortPair(listenPair)) {
-        cerr << "ERROR: invalid ip/port pair: " << vm["listen"].as<string>() << endl;
+        ERROR("invalid ip/port pair: " << vm["listen"].as<string>());
         exit(3);
     }
 
@@ -123,15 +172,15 @@ int main(int argc, char** argv) {
     for (vector<string>::iterator it = exports.begin(); it != exports.end(); it++) {
         IpPortPair p = splitIntoIpPortPair(*it);
         if (!checkIpPortPair(p)) {
-            cerr << "ERROR: invalid ip/port pair: " << *it << endl;
+            ERROR("invalid ip/port pair: " << *it);
             exit(3);
         }
         exportPairs.push_back(p);
     }
 
     if (verbosity > 1) {
-        cout << "listening on ip:" << listenPair.first << " port:" << listenPair.second << endl;
-        cout << "exporting to:";
+        cout << "DEBUG: listening on ip:" << listenPair.first << " port:" << listenPair.second;
+        cout << "DEBUG: exporting to:";
         for (vector<IpPortPair>::iterator it = exportPairs.begin(); it != exportPairs.end(); it++) {
             cout << " ip:" << it->first << " port:" << it->second;
         }
@@ -144,7 +193,7 @@ int main(int argc, char** argv) {
     
     try {
         listenSocket = Socket::UDPSocket();
-    } catch (string s) {
+    } catch (string & s) {
         cerr << s << endl;
         exit(3);
     }
@@ -155,7 +204,7 @@ int main(int argc, char** argv) {
 
     try {
         sendSocket = Socket::UDPSocket();
-    } catch (string s) {
+    } catch (string & s) {
         cerr << s << endl;
         exit(3);
     }
@@ -218,6 +267,9 @@ int main(int argc, char** argv) {
         // look if we had the sequence number - 1 in our window... worst-case: O(n) best-case: O(1)
         for (unsigned int i = lastSequenceIdx; i != (lastSequenceIdx-1)%SEQLEN; i = (i+1)%SEQLEN) {
             if ((sequenceNumber[i]+1) == newSequence) {
+                foundIdx = i;
+                break;
+            } else if ((sequenceNumber[i] > newSequence)) {
                 foundIdx = i;
                 break;
             }

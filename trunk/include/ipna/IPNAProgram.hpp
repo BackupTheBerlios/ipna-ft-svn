1c2316b4cdfbcb95df816ef194626b7ce8e727b0
alex <alex@0e9dafd1-d605-0410-a1ff-fb02f8364bd5>
2006-03-07 13:57:29 +0000

    added licence header

--- trunk/include/ipna/IPNAProgram.hpp
+++ trunk/include/ipna/IPNAProgram.hpp
@@ -1,3 +1,23 @@
+/*
+  IPNAProgram.hpp - a class that represents a complete program,
+                    holding some common data.
+
+  Copyright (C) 2006 Alexander Petry
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
 #ifndef IPNA_PROGRAM_HPP
 #define IPNA_PROGRAM_HPP
 
--- trunk/src/capture/Formatter.cc
+++ trunk/src/capture/Formatter.cc
@@ -1,3 +1,23 @@
+/*
+  Formatter.cc - a class to format records.
+
+  Copyright (C) 2006 Alexander Petry
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2, or (at your option)
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
 #include <ipna/capture/Formatter.hpp>
 
 using namespace ipna;
@@ -40,29 +60,13 @@ Formatter::format(ipna::parser::Record::RecordPtr record, std::ostream& os) {
       default:
 	assert(false);
       }
-      os << '\t';
     } else {
-
       os << (unsigned int)0;
     }
+
+    os << '\t';
   }
 
   os << std::endl;
   return os;
-  
-  /*  
-  os
-    << record->get(10)->asUInt() << '\t'
-    << record->get(8)->asIp()    << '\t'
-    << record->get(9)->asUInt() << '\t'
-    << record->get(14)->asUInt() << '\t'
-    << record->get(12)->asIp()   << '\t'
-    << record->get(13)->asUInt() << '\t'
-    << record->get(4)->asUInt()  << '\t'
-    << record->get(7)->asUInt()  << '\t'
-    << record->get(11)->asUInt() << '\t'
-    << record->get(2)->asUInt()  << '\t'
-    << record->get(1)->asUInt()
-    << std::endl;
-  */
 }
